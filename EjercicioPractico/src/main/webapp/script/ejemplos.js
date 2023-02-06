a84c96c59e71be2fbaf469acc501c5a8231261d1
DESKTOP-LHCE2F2\csi22 <manuelclarobarba@gmail.com>
2023-02-06 09:38:17 +0100

    no message

---
+++ EjercicioPractico/src/main/webapp/script/ejemplos.js
@@ -0,0 +1,15 @@
+function ejercicio() {
+	
+	//Datos principales:
+	let media = ((4.5 + 2.1) / 2);
+	let alumno = "C.C";
+	
+	//Cookies:
+	document.cookie = "Carmen Carrera";
+	
+	//Interfaz:
+	document.write("Alumno: "+ alumno + ". " + "Media: " + media + " " + typeof(media));
+}
+
+	//Alerta:
+	window.alert("");
